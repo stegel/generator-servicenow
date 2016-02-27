8eea40a42886d903ad1708ce5d90a1999604a610
AJ Siegel <aj.siegel@servicenow.com>
2016-02-27 18:42:45 -0500

    grab hashed user/pass from config and use to log in

--- generators/page/index.js
+++ generators/page/index.js
@@ -12,9 +12,16 @@ var PageGenerator = yeoman.generators.NamedBase.extend({
 		var query = 'name%3D' + this.name;
 		var yeo = this;
 		var done= this.async();
+
+		// unhash username and pasword
+		var authHash = new Buffer(this.config.get("authHash"), 'base64').toString("ascii");
+
+		this.username = authHash.substring(0,authHash.indexOf(":"));
+		this.password = authHash.substring(authHash.indexOf(":")+1);
+
 		rest.get("https://scdevelopment.service-now.com/api/now/table/sys_ui_page?sysparm_query=" + query, {
-				username : this.config.get('username');
-				password : this.config.get('password');
+				username : this.username,
+				password : this.password
 			}).on("complete",function(response){
 			if( response instanceof Error){
 				yeo.log("Error");
