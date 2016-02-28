bb14d42aaca778c9bb214d4f36255256277c1ff1
AJ Siegel <aj.siegel@servicenow.com>
2016-02-28 17:43:44 -0500

    sample of getting an using oauth token

---
+++ pageOAuth/index.js
@@ -0,0 +1,54 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var rest = require('restler');
+var SnClient = require("../snclient.js");
+
+var PageGenerator = yeoman.generators.NamedBase.extend({
+	initializing : function(){
+		var yeo = this;
+		var done = this.async();
+
+		var authHash = new Buffer(this.config.get("authHash"), 'base64').toString("ascii");
+
+
+		rest.post("https://scdevelopment.service-now.com/oauth_token.do",{
+			headers : {
+				"Accept" : "application/json"
+			},
+			data : {
+				grant_type : "password",
+				client_id : this.config.get("client_id"),
+				client_secret : this.config.get("client_secret"),
+				username : authHash.substring(0,authHash.indexOf(":")),
+			password : authHash.substring(authHash.indexOf(":")+1)
+			}
+		}).on("complete",function(data, response){
+			yeo.config.set("token",data.access_token);
+			yeo.config.save();
+
+			rest.post("https://scdevelopment.service-now.com/api/now/table/sys_user",{
+				headers : {
+					"Accept" : "application/json",
+					"Content-Type" : "application/json"
+				},
+				data : JSON.stringify({
+					user_name : yeo.name
+				}),
+				accessToken : data.access_token
+			}).on("complete",function(data,response){
+				if(data.error){
+					var mesage =
+					yeo.log(yosay("Error\nMessage: "+ data.error.message + "\nDetail: " + data.error.detail));
+
+					done();
+				}
+
+				done();
+			});
+		});
+	}
+});
+
+module.exports = PageGenerator
