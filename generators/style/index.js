93d7e92a224c89b60f1912202b9e3343b1a9bec8
AJ Siegel <aj.siegel@servicenow.com>
2016-02-28 14:10:46 -0500

    updated style generator to use snclient and better yeo structure

--- generators/style/index.js
+++ generators/style/index.js
@@ -3,78 +3,92 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var rest = require('restler');
+var SnClient = require("../../snclient.js");
 
 var StyleGenerator = yeoman.generators.NamedBase.extend({
 	initializing : function(){
-		var endpoint = this.config.get("endpoint");
-		var query = 'name%3D' + this.name;
+
+		this.query = 'name%3D' + this.name;
+
 		var yeo = this;
 		var done= this.async();
 
 		// unhash username and pasword
 		var authHash = new Buffer(this.config.get("authHash"), 'base64').toString("ascii");
 
-		this.username = authHash.substring(0,authHash.indexOf(":"));
-		this.password = authHash.substring(authHash.indexOf(":")+1);
+		var config = {
+			endpoint : this.config.get("endpoint"),
+			username : authHash.substring(0,authHash.indexOf(":")),
+			password : authHash.substring(authHash.indexOf(":")+1)
+		};
 
-		rest.get(endpoint + "/table/content_css?sysparm_query=" + query, {
-			headers : {
-				'Content-Type': 'application/json',
-				'Accepts': 'application/json'
-			},
-			username : this.username,
-			password : this.password
-			}).on("complete",function(response){
+		this.snClient = new SnClient(config);
+
+		this.snClient.getRecord("content_css",this.query).on("complete",function(response){
 			if( response.status == "failure"){
 				var mesage =
 				yeo.log(yosay("Error\nMessage: "+ response.error.message + "\nDetail: " + response.error.detail));
 			}
 			else{
-				var results = response.result;
+				yeo.results = response.result;
 
-				if (results.length === 0){
-					yeo.prompt([{
-						type : 'confirm',
-						name : 'createNewStyle',
-						message : 'This style sheet does not exist, do you want to create a new style sheet?',
-						default : false
-					}],function(answers){
-						if(answers.createNewStyle){
-
-							var jsonData = JSON.stringify({name : yeo.name});
-							rest.post(endpoint + "/table/content_css",{
-								headers : {
-									"Accept" : "application/json",
-									"Content-Type" : "application/json"
-								},
-								data : jsonData,
-								username : yeo.username,
-								password : yeo.password
-							}).on("complete", function(data, response){
-								if(response.statusCode == 200 || response.statusCode == 201){
-									yeo.fs.copyTpl(
-										yeo.templatePath('_style.css'),
-										yeo.destinationPath('app/css/' + yeo.name + '.css'),
-										{ content : ""}
-									);
-									done();
-								}
-
-							});
-						}
-					}.bind(yeo));
-
+				if (yeo.results.length === 0){
+					yeo.recordExists = false;
+					done();
 				}
 				else{
-					yeo.fs.copyTpl(
-						yeo.templatePath('_style.css'),
-						yeo.destinationPath('app/css/' + results[0].sys_name + '.css'),
-						{ content : results[0].style }
-					);
+					yeo.recordExists = true;
 					done();
+
 				}
 			}
 		});
+	},
+	prompting : function() {
+		var done = this.async();
+
+		if(!this.recordExists){
+			this.prompt([{
+				type : 'confirm',
+				name : 'createNewStyle',
+				message : 'This style sheet does not exist, do you want to create a new style sheet?',
+				default : false
+			}],function(answers){
+				if(answers.createNewStyle){
+					this.props = answers;
+					done();
+				}
+			}.bind(this));
+		}
+		else{
+			done();
+		}
+	},
+	writing : function() {
+		var yeo = this;
+
+		var done = this.async();
+
+		if(!this.recordExists && this.props.createNewStyle){
+			this.snClient.postRecord("content_css",{name : yeo.name}).on("complete", function(data, response){
+				if(response.statusCode == 200 || response.statusCode == 201){
+					yeo.fs.copyTpl(
+						yeo.templatePath('_style.css'),
+						yeo.destinationPath('app/css/' + yeo.name + '.css'),
+						{ content : ""}
+					);
+					done();
+				}
+			});
+		}
+		else{
+			this.fs.copyTpl(
+				this.templatePath('_style.css'),
+				this.destinationPath('app/css/' + this.results[0].sys_name + '.css'),
+				{ content : this.results[0].style }
+			);
+			done();
+		}
 	}
 });
 
