7f11ac2d7b305a12c1ccf219559df89abb21aadf
AJ Siegel <aj.siegel@servicenow.com>
2016-02-29 16:20:04 -0500

    tokens not rely on passing generator object

--- tokens.js
+++ tokens.js
@@ -3,15 +3,16 @@ var rest = require('restler');
 var jsonfile = require('jsonfile');
 
 module.exports = {
-	getToken : function(yeo){
+	getNewTokenFromRefresh : function(){
 
+		var oauthJSON = jsonfile.readFileSync(".oauth");
 		var data = {
 			grant_type : "refresh_token",
-			client_id : yeo.config.get("client_id"),
-			client_secret : yeo.config.get("client_secret"),
-			refresh_token : yeo.oauthJSON.refresh_token
-
+			client_id : oauthJSON.client_id,
+			client_secret : oauthJSON.client_secret,
+			refresh_token : oauthJSON.refresh_token
 		};
+
 		rest.post("https://scdevelopment.service-now.com/oauth_token.do",{
 			headers : {
 				"Accept" : "application/json"
@@ -20,16 +21,16 @@ module.exports = {
 		}).on("complete",function(data,response){
 			if( data.error){
 				var mesage =
-				yeo.log(yosay("Error\nMessage: "+ data.error + "\nDetail: " + data.error_description));
+				console.log("Error\nMessage: "+ data.error + "\nDetail: " + data.error_description);
 				done();
 			}
 			else{
-				yeo.oauthJSON.access_token = data.access_token;
-				jsonfile.writeFile('.oauth',yeo.oauthJSON,{spaces : 4},function(err){
+				oauthJSON.access_token = data.access_token;
+				jsonfile.writeFile('.oauth',oauthJSON,{spaces : 4},function(err){
 					if(err){
 						return console.log(err);
 					}
-					yeo.env.error("We had to fetch a new oauth key, please run again.");
+					return true;
 				});
 
 			}
