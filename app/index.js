42f1818066494d0867dcb3a25081ae563749e80b
AJ Siegel <aj.siegel@servicenow.com>
2016-03-03 17:00:24 -0500

    updated main task to use helpers folder, put the authHash into props and create the package, gruntfile, and sn-config

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 var rest = require('restler');
 var mkdirp = require('mkdirp');
-var SnClient = require("../snclient.js");
+var SnClient = require("../helpers/snclient.js");
 //var jsonfile = require('jsonfile');
 //var tokens = require('../tokens.js');
 
@@ -14,7 +14,7 @@ module.exports = yeoman.generators.Base.extend({
 	prompting : function() {
 		var done = this.async();
 		var yeo = this;
-		
+
 		this.prompt([{
 			type : 'input',
 			name : 'hostname',
@@ -42,8 +42,14 @@ module.exports = yeoman.generators.Base.extend({
 			name : 'appName',
 			message : 'What is your app prefix?',
 			default : 'solution'
+		},{
+			type : 'input',
+			name : 'dest',
+			message : 'Where do you want to store your app files',
+			default : 'dist'
 		}], function(answers){
 			this.props = answers;
+			this.props.authHash = new Buffer(this.props.username + ":" + this.props.password).toString("base64");
 			// TODO: implement OAuth2 better
 //			if(this.props.authType !== "Basic Authentication"){
 //				this.prompt([{
@@ -71,17 +77,11 @@ module.exports = yeoman.generators.Base.extend({
 		var done = this.async();
 		var yeo = this;
 
-		// encode username and pass
-		var authHash = new Buffer(this.props.username + ":" + this.props.password).toString("base64");
-		this.config.set({
-
-		});
-
 		this.config.set({
 			"hostname" : this.props.hostname,
 			"endpoint" : "https://" + this.props.hostname + ".service-now.com/api/now",
 //			"authType" : this.props.authType TODO implement OAuth v2
-			"authHash" : authHash
+			"authHash" : this.props.authHash
 		});
 
 
@@ -91,7 +91,7 @@ module.exports = yeoman.generators.Base.extend({
 		// setup snClient
 		var config = {
 			endpoint : this.config.get("endpoint"),
-			authHash : new Buffer(authHash,"base64").toString("ascii"),
+			authHash : new Buffer(this.props.authHash,"base64").toString("ascii"),
 //			authType : this.props.authType, TODO implement OAuth v2
 			client_id : this.props.client_id,
 			client_secret : this.props.client_secret
@@ -125,17 +125,29 @@ module.exports = yeoman.generators.Base.extend({
 	},
 
 	writing : function(){
-		var done = this.async();
-		var yeo = this;
+		var done = this.async(),
+			yeo = this,
+			dest = this.props.dest,
+			query = 'nameSTARTSWITH' + 	this.props.appName;
 
-		var query = 'nameSTARTSWITH' + 	this.props.appName;
-
-		mkdirp('app');
+		mkdirp(dest);
 		
 		// get UI Pages
-		mkdirp('app/ui_pages');
+		mkdirp(dest +'/ui_pages');
+		mkdirp(dest +'/ui_scripts');
+		mkdirp(dest +'/stylesheets');
 		
 		this.template('_package.json', 'package.json',{appName : this.props.appName})
+
+		this.template("_gruntfile.js", "Gruntfile.js",{
+			dest : dest
+		});
+
+		this.template("_sn-config.json",".sn-config.json",{
+			host : this.props.hostname,
+			auth : this.props.authHash,
+			prefix : this.props.appName
+		});
 		this.snClient.getRecord('sys_ui_page',query).on('complete', function(response) {
 			if( response.status == "failure"){
 				var mesage =
@@ -149,13 +161,12 @@ module.exports = yeoman.generators.Base.extend({
 				for(var i = 0; i < results.length; i++){
 					yeo.fs.copyTpl(
 						yeo.templatePath('_test.html'),
-						yeo.destinationPath('app/ui_pages/' + results[i].sys_name + '.html'),
+						yeo.destinationPath(dest + '/ui_pages/' + results[i].sys_name + '.html'),
 						{ content : results[i].html}
 					);
 				}
 			}
 			// get UI Scripts
-			mkdirp('app/ui_scripts');
 			yeo.snClient.getRecord('sys_ui_script',query).on('complete',function(response) {
 				if( response.status == "failure"){
 					var mesage =
@@ -170,11 +181,10 @@ module.exports = yeoman.generators.Base.extend({
 					for(var i = 0; i < results.length; i++){
 						yeo.fs.copyTpl(
 							yeo.templatePath('_test.js'),
-							yeo.destinationPath('app/ui_scripts/' + results[i].sys_name + '.js'),
+							yeo.destinationPath(dest + '/ui_scripts/' + results[i].sys_name + '.js'),
 							{ content : results[i].script}
 						);
 					}
-					mkdirp('app/css');
 					yeo.snClient.getRecord('content_css',query).on('complete',function(response) {
 						if( response.status == "failure"){
 							var mesage =
@@ -188,15 +198,11 @@ module.exports = yeoman.generators.Base.extend({
 							for(var i = 0; i < results.length; i++){
 								yeo.fs.copyTpl(
 									yeo.templatePath('_main.css'),
-									yeo.destinationPath('app/css/' + results[i].sys_name + '.css'),
+									yeo.destinationPath(dest + '/css/' + results[i].sys_name + '.css'),
 									{ content : results[i].style}
 								);
 							}
-							
-							yeo.fs.copyTpl(yeo.templatePath('_package.json'),yeo.destinationPath('package.json'),{
-								appName : yeo.props.appName
-							});
-							
+
 							done();
 						}
 					});
