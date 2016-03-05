7f7494f7d827ea9c80612bd4427567dc656ed363
AJ Siegel <aj.siegel@servicenow.com>
2016-03-05 14:04:03 -0500

    removed a bunch of dependencies

--- app/index.js
+++ app/index.js
@@ -31,87 +31,87 @@ Generator.prototype.welcome = function welcome(){
 	}
 };
 
-Generator.prototype.askForHostname = function askForHostname(){
-	var done = this.async();
-
-	this.prompt([{
-		type : 'input',
-		name : 'hostname',
-		message : 'What instance are you on? (the part before .service-now.com)',
-		default : 'scdevelopment'
-	}], function(props){
-		this.hostname = props.hostname;
-		done();
-	}.bind(this));
-};
-
-Generator.prototype.askForUsername = function askForUsername(){
-	var done = this.async();
-
-	this.prompt([{
-		type : 'input',
-		name : "username",
-		message : "What is your username?",
-	}], function(props){
-		this.username = props.username;
-		done();
-	}.bind(this));
-};
-
-Generator.prototype.askForPassword = function askForPassword(){
-	var done = this.async();
-
-	this.prompt([{
-		type : 'password',
-		name : "password",
-		message : "What is your password?",
-	}], function(props){
-		this.password = props.password;
-		this.authHash = new Buffer(this.username + ":" + this.password).toString("base64");
-		done();
-	}.bind(this));
-};
-
-Generator.prototype.askForPrefix = function askForPrefix(){
-	var done = this.async();
-
-	this.prompt([{
-		type : 'input',
-		name : 'appPrefix',
-		message : 'What is your app prefix?',
-		default : 'solution'
-	}], function(props){
-		this.appPrefix = props.appPrefix;
-		
-		done();
-	}.bind(this));
-
-};
-
-Generator.prototype.askForDest = function askForDest(){
-	var done = this.async();
-
-	this.prompt([{
-		type : 'input',
-		name : 'appDest',
-		message : 'Where do you want to store your app files?',
-		default : 'dist'
-	}], function(props){
-		this.appDest = props.appDest;
-
-		done();
-	}.bind(this));
-	
-};
-
-Generator.prototype.saveConfig = function saveConfig(){
-	this.template("_sn-config.json",".sn-config.json",{
-		host : this.hostname,
-		auth : this.authHash,
-		prefix : this.appPrefix,
-		dest : this.appDest
-	});
-};
+//Generator.prototype.askForHostname = function askForHostname(){
+//	var done = this.async();
+//
+//	this.prompt([{
+//		type : 'input',
+//		name : 'hostname',
+//		message : 'What instance are you on? (the part before .service-now.com)',
+//		default : 'scdevelopment'
+//	}], function(props){
+//		this.hostname = props.hostname;
+//		done();
+//	}.bind(this));
+//};
+//
+//Generator.prototype.askForUsername = function askForUsername(){
+//	var done = this.async();
+//
+//	this.prompt([{
+//		type : 'input',
+//		name : "username",
+//		message : "What is your username?",
+//	}], function(props){
+//		this.username = props.username;
+//		done();
+//	}.bind(this));
+//};
+//
+//Generator.prototype.askForPassword = function askForPassword(){
+//	var done = this.async();
+//
+//	this.prompt([{
+//		type : 'password',
+//		name : "password",
+//		message : "What is your password?",
+//	}], function(props){
+//		this.password = props.password;
+//		this.authHash = new Buffer(this.username + ":" + this.password).toString("base64");
+//		done();
+//	}.bind(this));
+//};
+//
+//Generator.prototype.askForPrefix = function askForPrefix(){
+//	var done = this.async();
+//
+//	this.prompt([{
+//		type : 'input',
+//		name : 'appPrefix',
+//		message : 'What is your app prefix?',
+//		default : 'solution'
+//	}], function(props){
+//		this.appPrefix = props.appPrefix;
+//
+//		done();
+//	}.bind(this));
+//
+//};
+//
+//Generator.prototype.askForDest = function askForDest(){
+//	var done = this.async();
+//
+//	this.prompt([{
+//		type : 'input',
+//		name : 'appDest',
+//		message : 'Where do you want to store your app files?',
+//		default : 'dist'
+//	}], function(props){
+//		this.appDest = props.appDest;
+//
+//		done();
+//	}.bind(this));
+//
+//};
+//
+//Generator.prototype.saveConfig = function saveConfig(){
+//	this.template("_sn-config.json",".sn-config.json",{
+//		host : this.hostname,
+//		auth : this.authHash,
+//		prefix : this.appPrefix,
+//		dest : this.appDest
+//	});
+//};
 
 Generator.prototype.setupPackage = function setupPackage(){
 	this.template("_package.json", "package.json",{
@@ -132,7 +132,10 @@ Generator.prototype.installers = function installers(){
       skipInstall: this.options['skip-install'],
       skipMessage: this.options['skip-message'],
 		 bower : false,
-      callback: function(){this.spawnCommand("grunt",["init"]);}.bind(this)
+      callback: function(){
+		  this.spawnCommandSync("grunt",["init"]);
+		  console.log("Sync");
+	  }.bind(this)
     });
 };
 
