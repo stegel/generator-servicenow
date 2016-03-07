3d7d0d9ff6a685b9b94abcafa771a52d7160caec
AJ Siegel <aj.siegel@servicenow.com>
2016-03-07 14:29:35 -0500

    updated to include grunt-contrib-watch

--- app/templates/_gruntfile.js
+++ app/templates/_gruntfile.js
@@ -3,8 +3,19 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		pull : [],
 		destination : "<%= dest %>",
-		appPrefix : "<%= appPrefix %>"
+		appPrefix : "<%= appPrefix %>",
+		watch : {
+			files : ["dist/ui_scripts/*.js","dist/ui_pages/__*.xhtml","dist/stylesheets/*.css"],
+			tasks : ['push:<%= folder %>:<%= watch_file_name %>']
+		}
+	});
+
+	grunt.event.on('watch', function(action, filepath) {
+
+		grunt.config("folder",path.relative(path.join(process.cwd(),"dist"),path.dirname(filepath)));
+		grunt.config("watch_file_name",path.basename(filepath,path.extname(filepath)).replace(grunt.config("appPrefix"),""));
 	});
 
 	grunt.loadNpmTasks("grunt-servicenow");
+	grunt.loadNpmTasks("grunt-contrib-watch")
 };
